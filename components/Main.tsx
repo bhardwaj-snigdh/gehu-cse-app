6985c08bd91a15e6c84d5f1937f04f5a7a8413c3
Snigdh Bhardwaj <59894983+bhardwaj-snigdh@users.noreply.github.com>
2022-07-02 11:43:52 +0530

    feat: :children_crossing: load user from storage on booting

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,21 +1,20 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Layout, Spinner } from '@ui-kitten/components';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { Keyboard } from 'react-native';
 import { setVisible } from '../redux/feature/keyboard/keyboardSlice';
-import { login } from '../redux/feature/user/userSlice';
-import { useAppDispatch } from '../redux/hooks';
-import type { User } from '../types/UserAndRoles';
+import { loadFomStorage, selectAuthBooting, selectUser } from '../redux/feature/user/userSlice';
+import { useAppDispatch, useAppSelector } from '../redux/hooks';
 import AppNavigationTabs from './AppNavigationTabs';
 import AuthNavigationStack from './AuthNavigationStack';
 
 export default function Main() {
-  const [loading, setLoading] = useState(true);
-  const [user, setUser] = useState<User | null>(null);
-  const [token, setToken] = useState<string | null>(null);
+  const booting = useAppSelector(selectAuthBooting);
+  const user = useAppSelector(selectUser);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
+    dispatch(loadFomStorage());
+
     const showSubscription = Keyboard.addListener('keyboardDidShow', () => {
       dispatch(setVisible(true));
     });
@@ -29,22 +28,7 @@ export default function Main() {
     };
   }, [dispatch]);
 
-  useEffect(() => {
-    setLoading(true);
-    AsyncStorage.getItem('auth').then((res) => {
-      if (!res) {
-        setLoading(false);
-      } else {
-        setLoading(false);
-        const authObject = JSON.parse(res);
-        setUser(authObject.user);
-        setToken(authObject.token);
-        dispatch(login({ user, token }));
-      }
-    });
-  }, [dispatch, token, user]);
-
-  if (loading) {
+  if (booting) {
     return (
       <Layout style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Spinner size="giant" status="basic" />
